2981501d8ad6e0605e1bebd53703509f137a6825
ajabri <ajabri@gmail.com>
2020-03-26 23:57:39 -0700

    add masker

--- moco/builder.py
+++ moco/builder.py
@@ -8,7 +8,7 @@ class MoCo(nn.Module):
     Build a MoCo model with: a query encoder, a key encoder, and a queue
     https://arxiv.org/abs/1911.05722
     """
-    def __init__(self, base_encoder, dim=128, K=65536, m=0.999, T=0.07, mlp=False):
+    def __init__(self, base_encoder, masker, dim=128, K=65536, m=0.999, T=0.07, mlp=False):
         """
         dim: feature dimension (default: 128)
         K: queue size; number of negative keys (default: 65536)
@@ -31,11 +31,15 @@ class MoCo(nn.Module):
             self.encoder_q.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.encoder_q.fc)
             self.encoder_k.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.encoder_k.fc)
 
+        self.masker = masker
+
         for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
             param_k.data.copy_(param_q.data)  # initialize
             param_k.requires_grad = False  # not update by gradient
 
         # create the queue
+        # print(K)
+
         self.register_buffer("queue", torch.randn(dim, K))
         self.queue = nn.functional.normalize(self.queue, dim=0)
 
@@ -157,8 +161,12 @@ class MoCo(nn.Module):
         # dequeue and enqueue
         self._dequeue_and_enqueue(k)
 
-        return logits, labels
+        # mask stuff
+        masks, logits_m, m_aux_loss = self.masker(q, k)
+        logits_m = torch.cat([logits_m[:, None], l_pos], dim=1)
+        labels_m = torch.zeros(logits_m.shape[0], dtype=torch.long).cuda()
 
+        return logits, labels, (logits_m, labels_m, masks, m_aux_loss)
 
 # utils
 @torch.no_grad()
